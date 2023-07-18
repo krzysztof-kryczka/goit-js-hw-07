f9e6c00646b503ca1d38bcd3fba54f027bb86962
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-07-18 16:05:34 +0200

    function: it shows a large picture when you click on the thumbnail

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,3 +19,25 @@ const genDivItem = ({ preview, original, description }) => `
 const getImages = (img) => img.map((item) => genDivItem(item)).join("");
 
 gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener("click", onClickGallery);
+
+const genBigImg = ({ url, description}) => `
+  <img
+    class="gallery__image"
+    src="${url}"
+    width="1280"
+    alt="${description}"/>
+`;
+
+function onClickGallery(e) {
+  e.preventDefault();
+
+  const url = e.target.dataset.source;
+  const description = e.target.alt;
+
+  if (!(url && description)) return;
+
+  const instance = basicLightbox.create(genBigImg({ url, description}));
+  instance.show();
+}
